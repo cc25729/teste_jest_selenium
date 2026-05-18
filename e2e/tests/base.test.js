8bcbac163786ff57ab25b2cd27855925a5a9bf15
cc25729 <cc25729@g.unicamp.br>
2026-05-18 16:53:50 -0300

    aaaaa
    aaaaaa

--- e2e/tests/base.test.js
+++ e2e/tests/base.test.js
@@ -11,9 +11,55 @@ if (!fs.existsSync(SCREENSHOTS_DIR)) fs.mkdirSync(SCREENSHOTS_DIR, {recursive: t
 
 async function tiraFoto(name){
     try{
-        
+        const img = await driver.takeScreenshot()
+        const filePath = path.join(SCREENSHOTS_DIR,`${name}.png`)
+        fs.writeFileSync(filePath, img, 'base64')
+        console.log(`Foto tirada ${name}.png`)
     }
     catch(err){
         console.warn('Erro ao tirar foto')
     }
 }
+
+async function tirarFoto(nome) {
+ 
+}
+async function main() {
+    try{
+        const opts = new chrome.Options()
+        opts.addArguments(
+            '--headless=new',
+            '--no-sangbox',
+            '--disiable-dev-shm-usage',
+            '--window-size=800, 640',
+            '--disable-gpu'
+        )
+
+        driver = await new Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(opts)
+            .build()
+
+        await driver.manage().setTimeouts({implicite: 5000, pageLoad: 5000})
+
+        await driver.get(BASE_URL+'/login')
+        tiraFoto('pagina_login')
+        await driver.findElement(By.id("username")).sendKeys('Adm')
+        await driver.findElement(By.id('password')).sendKeys('admin')
+
+        tiraFoto('Dados_preenchidos')
+        
+        await driver.findElement(By.id('loginForm').submit())
+        await new Promise(r => setTimeout(r, 800))
+
+        tiraFoto('pagina_erro')
+
+    }finally{
+        
+    }
+}
+
+main().catch( err => {
+    console.log('Erro fatal', err)
+    process.exit(1)
+})
